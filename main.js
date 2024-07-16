dfd7c7ab480d3756d0719113cbf8cf20780d05ba
prog8melawai <prog8melawai@gmail.com>
2024-07-16 17:46:15 +0700

    feat: api fetch

--- main.js
+++ main.js
@@ -26,3 +26,62 @@ const errorHandler = () => {
 
 //GET QUOTES FROM API
 // async function 
+
+// fetch ("http://api.allorigins.win/get?url=http://api.api-ninjas.com/vi/quotes")
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error("could not fetch resource");
+//         }
+//         return response.json();
+//     })
+//     .then(data=> console.log(data))
+//     .catch(error => console.error(error));
+
+fetchData();
+
+async function fetchData() {
+
+    try {
+        const response = await fetch(`https://api.quotable.io/quotes/random`);
+
+        if (!response.ok) {
+            throw new Error("could not fetch resource");
+        }
+
+        const data = await response.json();
+        console.log(data);
+        const obj = JSON.parse(data.contents);
+    
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
+
+// const proxyurl = "http://api.allorigins.win/get?url=";
+// const apiurl = "http://api.api-ninjas.com/vi/quotes";
+
+// fetch (`https://api.allorigins.win/get?url=${encodeURIComponent('https://api.quotable.io/quotes/random')}`, {
+//     method: 'get',
+// })
+//     .then(response => {
+//                 if (!response.ok) {
+//                     throw new Error("could not fetch resource");
+//                 }
+//                 return response.json();
+//             })
+//     .then(data  => console.log(data.contents))
+//     .catch(error => console.log(error));
+
+
+    // fetch ('https://api.quotable.io/quotes/random', {
+    //     method: 'get',
+    // })
+    //     .then(response => {
+    //                 if (!response.ok) {
+    //                     throw new Error("could not fetch resource");
+    //                 }
+    //                 return response.json();
+    //             })
+    //     .then(data  => console.log(data))
+    //     .catch(error => console.log(error));
